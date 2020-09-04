0948799c7a9589c15f0147efad8fadcfb2338e73
sufiyanshaikh317 <70483299+sufiyanshaikh317@users.noreply.github.com>
2020-09-04 22:41:29 +0530

    Add files via upload
    Day 2 assignment

---
+++ Minutestosec.js
@@ -0,0 +1,15 @@
+function convertMinutestoSeconds(minutes) 
+{
+  return Math.floor(minutes * 60);
+}
+
+var minutesToSeconds = convertMinutestoSeconds(3); 
+    document.write( " Minutes to seconds converted : " + minutesToSeconds );
+
+
+
+    
+
+         
+
+      
